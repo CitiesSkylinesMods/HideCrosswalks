c2826f3fec409eb21c520f6693339867c57e2a3d
kian.zarrin <kian.zarrin@gmail.com>
2019-12-11 10:13:27 +0200

    First commit untested

---
+++ HideTMPECrosswalks/Patches/HookBase.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using System.Reflection;
+
+namespace HideTMPECrosswalks.Patch {
+    public abstract class HookBase : MonoBehaviour {
+        private RedirectCallsState State = null;
+        public abstract MethodInfo From { get; }
+        public abstract MethodInfo To { get; }
+
+        public void Hook() {
+            if(State != null) {
+                Debug.Log("already hooked.");
+                return;
+            }
+            MethodInfo from = From;
+            MethodInfo to = To;
+            Debug.Log($"Hooking {STR(from)} to {STR(to)} ...");
+            if (From == null || To == null) {
+                Debug.LogError("hooking failed!");
+                return;
+            }
+            State = RedirectionHelper.RedirectCalls(from, to);
+        }
+        public void UnHook() {
+            if (State != null) {
+                MethodInfo from = From;
+                Debug.Log($"UnHooking {STR(from)} ...");
+                RedirectionHelper.RevertRedirect(from, State);
+                State = null;
+            }
+        }
+
+        public string STR(MethodInfo info)=> info == null? "null": $"{info?.DeclaringType?.FullName} :: {info}";
+    }
+}
