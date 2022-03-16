f353058ff43e45a22dcec0a6a8112b60dc2f5b61
kianzzarrin <kian.zarrin@gmail.com>
2022-03-16 02:46:10 +0200

    polipolish patch

--- HideTMPECrosswalks/Patches/CalculateMaterialCommons.cs
+++ HideTMPECrosswalks/Patches/CalculateMaterialCommons.cs
@@ -1,20 +1,13 @@
-using ICities;
-using HarmonyLib;
-using System.Collections.Generic;
-using System.Reflection.Emit;
-using System.Reflection;
-using System.Linq;
-
-using ColossalFramework;
-/* Notes
-
- */
- 
 namespace HideCrosswalks.Patches {
-    using System;
+    using HarmonyLib;
+    using System.Collections.Generic;
+    using System.Reflection.Emit;
+    using System.Reflection;
     using UnityEngine;
-    using Utils; using KianCommons;
-    using static TranspilerUtils;
+    using Utils;
+    using KianCommons;
+    using KianCommons.Patches;
+
     public static class CalculateMaterialCommons {
         public static bool ShouldHideCrossing(ushort nodeID, ushort segmentID) {
             // TODO move to netnode.updateflags or something
@@ -42,30 +35,21 @@ namespace HideCrosswalks.Patches {
             return material;
         }
 
-        static Type[] args = new [] {
-                typeof(Mesh),
-                typeof(Vector3),
-                typeof(Quaternion),
-                typeof(Material),
-                typeof(int),
-                typeof(Camera),
-                typeof(int),
-                typeof(MaterialPropertyBlock) };
-        static MethodInfo mDrawMesh =
-            typeof(Graphics).GetMethod("DrawMesh", args) ?? throw new Exception("mDrawMesh is null");
+        delegate void DrawMesh(Mesh mesh, Vector3 position, Quaternion rotation, Material material, int layer, Camera camera, int submeshIndex, MaterialPropertyBlock properties);
+        static MethodInfo mDrawMesh = typeof(Graphics).GetMethod<DrawMesh>(throwOnError: true);
         static FieldInfo fNodeMaterial =
-            typeof(NetInfo.Node).GetField("m_nodeMaterial") ?? throw new Exception("fNodeMaterial is null");
+            ReflectionHelpers.GetField<NetInfo.Node>(nameof(NetInfo.Node.m_nodeMaterial));
         static MethodInfo mCalculateMaterial =
-            typeof(CalculateMaterialCommons).GetMethod("CalculateMaterial") ?? throw new Exception("mCalculateMaterial is null");
+            typeof(CalculateMaterialCommons).GetMethod("CalculateMaterial", throwOnError: true);
         static MethodInfo mCheckRenderDistance =
-            typeof(RenderManager.CameraInfo).GetMethod("CheckRenderDistance") ?? throw new Exception("mCheckRenderDistance is null");
+            typeof(RenderManager.CameraInfo).GetMethod("CheckRenderDistance", throwOnError: true);
         static MethodInfo mShouldHideCrossing =
-            typeof(CalculateMaterialCommons).GetMethod("ShouldHideCrossing") ?? throw new Exception("mShouldHideCrossing is null");
+            typeof(CalculateMaterialCommons).GetMethod("ShouldHideCrossing", throwOnError: true);
         static MethodInfo mGetSegment =
-            typeof(NetNode).GetMethod("GetSegment") ?? throw new Exception("mGetSegment is null");
+            typeof(NetNode).GetMethod("GetSegment", throwOnError: true);
 
         // returns the position of First DrawMesh after index.
-        public static void PatchCheckFlags(List<CodeInstruction> codes, int occurance, MethodInfo method) {
+        public static void PatchCheckFlags(List<CodeInstruction> codes, int occurance, MethodBase method) {
             //Assertion.Assert(mDrawMesh != null, "mDrawMesh!=null failed");
             //Assertion.Assert(fNodeMaterial != null, "fNodeMaterial!=null failed"); 
             //Assertion.Assert(mCalculateMaterial != null, "mCalculateMaterial!=null failed"); 
@@ -73,22 +57,22 @@ namespace HideCrosswalks.Patches {
             //Assertion.Assert(mShouldHideCrossing != null, "mShouldHideCrossing!=null failed");
 
             int index = 0;
-            index = SearchInstruction(codes, new CodeInstruction(OpCodes.Call, mDrawMesh), index, counter: occurance);
+            index = codes.Search(c => c.Calls(mDrawMesh), startIndex: index, count: occurance);
             Assertion.Assert(index != 0, "index!=0");
 
 
             // find ldfld node.m_material
-            index = SearchInstruction(codes, new CodeInstruction(OpCodes.Ldfld, fNodeMaterial), index, dir: -1);
+            index = codes.Search(c => c.LoadsField(fNodeMaterial), startIndex: index, count: -1);
             int insertIndex2 = index + 1;
 
             // find: if (cameraInfo.CheckRenderDistance(data.m_position, node.m_lodRenderDistance))
             /* IL_0627: callvirt instance bool RenderManager CameraInfo::CheckRenderDistance(Vector3, float32)
              * IL_062c brfalse      IL_07e2 */
-            index = SearchInstruction(codes, new CodeInstruction(OpCodes.Callvirt, mCheckRenderDistance), index, dir: -1);
+            index = codes.Search(c => c.Calls(mCheckRenderDistance), startIndex: index, count: -1);
             int insertIndex1 = index + 1; // at this point boloean is in stack
 
 
-            CodeInstruction LDArg_NodeID = GetLDArg(method, "nodeID"); // push nodeID into stack
+            CodeInstruction LDArg_NodeID = TranspilerUtils.GetLDArg(method, "nodeID"); // push nodeID into stack
             CodeInstruction LDLoc_segmentID = BuildSegnentLDLocFromPrevSTLoc(codes, index, counter: 1); // push segmentID into stack
 
             { // Insert material = CalculateMaterial(material, nodeID, segmentID)
@@ -97,7 +81,7 @@ namespace HideCrosswalks.Patches {
                     LDLoc_segmentID,
                     new CodeInstruction(OpCodes.Call, mCalculateMaterial), // call Material CalculateMaterial(material, nodeID, segmentID).
                 };
-                InsertInstructions(codes, newInstructions, insertIndex2);
+                codes.InsertInstructions(insertIndex2, newInstructions);
             }
 
             { // Insert ShouldHideCrossing(nodeID, segmentID)
@@ -106,19 +90,19 @@ namespace HideCrosswalks.Patches {
                     LDLoc_segmentID, 
                     new CodeInstruction(OpCodes.Call, mShouldHideCrossing), // call Material mShouldHideCrossing(nodeID, segmentID).
                     new CodeInstruction(OpCodes.Or) };
-
-                InsertInstructions(codes, newInstructions, insertIndex1);
+                codes.InsertInstructions(insertIndex1, newInstructions);
             } // end block
         } // end method
 
         public static CodeInstruction BuildSegnentLDLocFromPrevSTLoc(List<CodeInstruction> codes, int index, int counter=1) {
             Assertion.Assert(mGetSegment != null, "mGetSegment!=null");
-            index = SearchInstruction(codes, new CodeInstruction(OpCodes.Call, mGetSegment), index, counter: counter, dir: -1);
+            index = codes.Search(c => c.Calls(mGetSegment), startIndex: index, count: -counter);
 
             var code = codes[index + 1];
-            Assertion.Assert(IsStLoc(code), $"IsStLoc(code) | code={code}");
+            Assertion.Assert(code.IsStloc(), $"IsStLoc(code) | code={code}");
 
-            return BuildLdLocFromStLoc(code);
+            return code.BuildLdLocFromStLoc();
+            
         }
 
 
--- HideTMPECrosswalks/Patches/NetNodePatches/RenderInstance.cs
+++ HideTMPECrosswalks/Patches/NetNodePatches/RenderInstance.cs
@@ -1,37 +1,23 @@
-using HarmonyLib;
-using System;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Collections.Generic;
-
 namespace HideCrosswalks.Patches.NetNodePatches {
-    using Utils; using KianCommons;
+    using HarmonyLib;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Reflection.Emit;
+    using KianCommons;
+    using System.Linq;
 
     [HarmonyPatch()]
     public static class RenderInstance {
         static void Log(string m) => KianCommons.Log.Info("NetNode_RenderInstance Transpiler: " + m);
 
-        // RenderInstance(RenderManager.CameraInfo cameraInfo, ushort nodeID, NetInfo info, int iter, Flags flags, ref uint instanceIndex, ref RenderManager.Instance data)
-        static MethodInfo Target = typeof(global::NetNode).GetMethod("RenderInstance", BindingFlags.NonPublic | BindingFlags.Instance);
-        static MethodBase TargetMethod() {
-            var ret = Target;
-            Assertion.Assert(ret != null, "did not manage to find original function to patch");
-            Log("aquired method " + ret);
-            return ret;
-        }
+        static MethodBase TargetMethod() =>
+            typeof(NetNode).GetMethod("RenderInstance", BindingFlags.NonPublic | BindingFlags.Instance, throwOnError: true);
 
         //static bool Prefix(ushort nodeID){}
-        public static IEnumerable<CodeInstruction> Transpiler(ILGenerator il, IEnumerable<CodeInstruction> instructions) {
-            try {
-                var codes = TranspilerUtils.ToCodeList(instructions);
-                CalculateMaterialCommons.PatchCheckFlags(codes, occurance: 2, Target);
-
-                Log("successfully patched NetNode.RenderInstance");
-                return codes;
-            }catch(Exception e) {
-                KianCommons.Log.Error(e.ToString());
-                throw e;
-            }
+        public static IEnumerable<CodeInstruction> Transpiler(ILGenerator il, IEnumerable<CodeInstruction> instructions, MethodBase origin) {
+            var codes = instructions.ToList();
+            CalculateMaterialCommons.PatchCheckFlags(codes, occurance: 2, origin);
+            return codes;
         }
     } // end class
 } // end name space
